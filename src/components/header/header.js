e4987b54ce93b3907925348c355a9f0a55806705
faizal <iamfaizalkhn@gmail.com>
2024-04-19 17:08:55 +0530

    add-landing-ui

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,33 +2,29 @@ function header() {
 
     return (
         <>
-            <div className='w-full bg-gradient-to-r-fuchsia'>
-                <nav className='md:flex md:flex-row  flex-row w-full justify-between'>
-                    <div className=' basis-1/5'>
-                        <div>
-                            <img alt='pandao logo' />
-                        </div>
-
+            <div className='w-full bg-purple-900 '>
+                <nav className='md:flex md:flex-row  flex flex-row w-full justify-between p-0 m-0 md:place-items-start'>
+                    <div className='md:basis-1/5 flex flex-row md:justify-center w-1/2'>
+                        <img alt='pandao logo' src={process.env.PUBLIC_URL + "/images/logo.png"}className='h-16' />
                     </div>
-                    <div className='hidden md:flex  basis-2/5 items-center justify-center pl-10 pr-10 '>
-                        <div className='flex flex-row justify-between w-full px-2'>
-                            <span className='text-2xl font-bold hover:scale-110 transition duration-300 ease-in-out'>Resources</span>
-                            <span className='text-2xl font-bold '>|</span>
-                            <span className='text-2xl font-bold hover:scale-110 transition duration-300 ease-in-out'>Products</span>
-                            <span className='text-2xl font-bold '>|</span>
-                            <span className='text-2xl font-bold hover:scale-110 transition duration-300 ease-in-out'>About us</span>
+                    <div className='hidden md:flex  md:basis-2/5 justify-left pt-4 pr-0 '>
+                        <div className='flex flex-row justify-center w-full px-2'>
+                            <span className='px-2 text-2xl  underline font-bold hover:scale-110 transition duration-300 ease-in-out'>Resources</span>
+                            <span className='px-2 text-2xl font-bold '>|</span>
+                            <span className='px-2 text-2xl underline font-bold hover:scale-110 transition duration-300 ease-in-out'>Products</span>
+                            <span className='px-2 text-2xl font-bold '>|</span>
+                            <span className='px-2 text-2xl   font-bold hover:scale-110 transition duration-300 ease-in-out'>About us</span>
                         </div>
                     </div>
-                    <div className=' basis-2/5 flex flex-row items-center justify-between'>
-                        <div className='w-1/2 flex flex-row justify-center'>
-                            <button className='rounded-xl w-2/3 hover:scale-110 transition duration-300 ease-in-out' >
-                                <span className='text-2xl font-bold '>What is dao</span>
+                    <div className='md:basis-2/5 flex flex-row pl-0 pr-0 w-1/2'>
+                        <div className='w-full flex flex-row justify-center'>
+                            <button className='rounded-xl md:w-2/3 hover:scale-110 transition duration-300 ease-in-out h-10 px-5 overflow-hidden ' >
+                                What is dao?
                             </button>
-                                
                         </div>
-                        <div className='w-1/2 items-center flex-row justify-center'>
-                            <button className='rounded-xl w-2/3 hover:scale-110 transition duration-300 ease-in-out'>
-                                <span className='text-2xl font-bold '>Launch DAO</span>
+                        <div className='flex w-full flex-row justify-center items-center'>
+                            <button className='rounded-xl md:w-2/3 hover:scale-110 transition duration-300 ease-in-out h-10 px-2 overflow-hidden'>
+                                Launch DAO
                             </button>
 
                         </div>
--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,12 +1,31 @@
 import Header from "../../components/header/header";
 
+function IntroText() {
+    return (
+        <>
+            <div className='w-full h-screen md:flex md:flex-col justify-center'>
+                <div className='flex flex-row w-1/2'>
+                    <div className='px-10'>
+                        <h1 className='text-white text-5xl'>
+                            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                        </h1>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </>
+    )
+}
 
 function LandingPage() {
     return (
         <>
-            <div >
-            <Header />
-                
+            <div className='p-0 m-0 bg-purple-900 h-full'>
+                <Header />
+                <IntroText />
             </div>
 
         </>
