73e3560bc192528f033a50e25d53cf6b6aca5e8f
faisal004 <faisalhusain1320@gmail.com>
2024-05-09 21:25:59 +0530

    added : user dashboard added

--- frontend/src/Pages/UserDashboard.jsx
+++ frontend/src/Pages/UserDashboard.jsx
@@ -1,7 +1,31 @@
+import { useAccount } from "@/AccountContext";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const UserDashboard = () => {
-    return ( <div className="pt-20 min-h-screen bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-[#281038] from-0% via-[#181734] via-50%  to-[#0D1E3B] to-100% text-white">
-        Hi
-    </div> );
-}
- 
+  const { accounts } = useAccount();
+
+  return (
+    <div className="pt-20 min-h-screen bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-[#281038] from-0% via-[#181734] via-50%  to-[#0D1E3B] to-100% text-white">
+      <Card className="mx-10">
+        <CardHeader>
+          <CardTitle>User Details</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{accounts[0].address}</p>
+        </CardContent>
+        <CardFooter>
+          <p>{accounts[0].label}</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
 export default UserDashboard;
--- frontend/src/Pages/components/Navbar.jsx
+++ frontend/src/Pages/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../../components/ui/button";
 import MobileSidebar from "./MobileSidebar";
 import { Rocket } from "lucide-react";
+import { useRdt } from "@/hooks/useRdt";
 
 
 
 const Navbar = () => {
-
+  const rdt = useRdt();
+  const navigate=useNavigate()
+  rdt.walletApi.provideConnectResponseCallback((result) => {
+    if (result.isErr()) {
+      console.log("ko")
+    }else{
+      navigate("/userDashboard")
+    }
+  })
   return (
     <div className="w-full p-4 bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-[#281038] from-0% via-[#181734] via-50%  to-[#0D1E3B] to-100%  border-b-2 border-purple-800  text-white fixed ">
       <div className="max-w-[1440px] mx-auto flex  items-center justify-between h-10   ">
