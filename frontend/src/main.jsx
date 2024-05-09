ab5f27abef09c9c5f4794d8baef1de3515a060d0
faisal004 <faisalhusain1320@gmail.com>
2024-05-09 21:03:52 +0530

    changee

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { GatewayApiClient } from "@radixdlt/babylon-gateway-api-sdk";
+import { RadixDappToolkit, RadixNetwork } from "@radixdlt/radix-dapp-toolkit";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import './index.css'
-import RadixProvider from './context/rtd.jsx'
+import { GatewayApiProvider } from "./context/GatewayApiProvider.jsx";
+import { RdtProvider } from "./context/RdtProvider.jsx";
+import { AccountProvider } from "./AccountContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+const dappConfig = {
+  networkId: RadixNetwork.Stokenet,
+  applicationVersion: "1.0.0",
+  applicationName: "Pandao",
+  applicationDappDefinitionAddress: "account_tdx_2_128wx75khgz5rstma5aapejz3656eluz43v3jruz2s3kv3n0urr8m23",
+};
+const rdt = RadixDappToolkit(dappConfig);
+console.log("dApp Toolkit: ", rdt);
+const gatewayApi = GatewayApiClient.initialize(dappConfig);
+console.log("gatewayApi: ", gatewayApi);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RadixProvider>
-     <App />
-
-     </RadixProvider>
-
-  </React.StrictMode>,
-)
+    <RdtProvider value={rdt}>
+      <GatewayApiProvider value={gatewayApi}>
+        <AccountProvider>
+          <App />
+        </AccountProvider>
+      </GatewayApiProvider>
+    </RdtProvider>
+  </React.StrictMode>
+);
