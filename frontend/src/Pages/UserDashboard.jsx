f04c34591d3bd20ad8b315d10c955194963cf380
faisal004 <faisalhusain1320@gmail.com>
2024-05-16 21:11:45 +0530

    user dashboard revamped

--- frontend/src/Pages/UserDashboard.jsx
+++ frontend/src/Pages/UserDashboard.jsx
@@ -8,46 +8,100 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Check, Copy } from "lucide-react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const UserDashboard = () => {
   const { accounts } = useAccount();
+  const [copied, setCopied] = useState(false);
   const navigate = useNavigate();
-
+  const handleCopy = (address) => {
+    navigator.clipboard.writeText(address);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
   if (!accounts || accounts.length === 0) {
-    navigate("/")
+    navigate("/");
     return null;
   }
 
   return (
-    <div className="pt-20 relative flex flex-col items-center gap-3 justify-start min-h-screen  bg-[url('/bg.svg')]  bg-cover bg-black p-7">
-      <Card className=" w-full flex items-center h-full mt-10 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100 max-w-[1440px]  ">
-        <CardHeader>
-          <Avatar className="h-40 w-40">
-            <AvatarImage
-              src="https://github.com/shadcn.png"
-              className="h-40 w-40"
-            />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </CardHeader>
-        <CardContent className="text-xl font-bold flex flex-col gap-2">
-          <p className="bg-purple-400 p-2 rounded-lg flex flex-wrap">
-            {accounts[0].address}
-          </p>
-          <p className="bg-purple-400 p-2 rounded-lg">{accounts[0].label}</p>
-        </CardContent>
-      </Card>
-      <div className="px-2 w-full flex items-center justify-end max-w-[1440px]  ">
-        <Button
-          onClick={() => {
-            navigate("/exploreDao");
-          }}
-          variant="outline"
-          className="text-black"
-        >
-          Explore DAO
-        </Button>
+    <div className="pt-20 relative flex  items-start gap-3 justify-start min-h-screen  bg-[url('/bg.svg')]  bg-cover bg-black p-7">
+      <div className="max-w-[1440px] flex w-full mx-auto gap-2">
+        <Card className=" w-[20%] flex flex-col items-center h-full mt-10 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30  max-w-[1440px]  ">
+          <CardHeader>
+            <Avatar className="h-40 w-40">
+              <AvatarImage
+                src="https://github.com/shadcn.png"
+                className="h-40 w-40"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </CardHeader>
+          <CardContent className="text-xl font-bold flex flex-col gap-2 w-full">
+            <div
+              className="bg-purple-400 py-1 px-2 rounded-lg flex flex-wrap text-ellipsis overflow-hidden relative group"
+              style={{ maxWidth: "100%" }}
+            >
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger> {accounts[0].address}</TooltipTrigger>
+                  <TooltipContent>
+                    {accounts[0].address}
+                  
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <button
+                      onClick={()=>handleCopy(accounts[0].address)}
+                      disabled={copied}
+                      className="py-0 bg-purple-500 text-white rounded-md px-2  h-6 text-xs absolute top-2 right-2 group-hover:block hidden "
+                    >
+                      {copied ? (
+                        <Check className="h-4 w-4" />
+                      ) : (
+                        <Copy className="h-4 w-4" />
+                      )}
+                    </button>
+            </div>
+
+            <p className="bg-purple-400 p-2 rounded-lg">{accounts[0].label}</p>
+          </CardContent>
+        </Card>
+        <div className=" w-[80%] mt-10 flex flex-col items-center justify-start ">
+          <div className="flex gap-2 items-center justify-end w-full border-b-2 border-gray-500 pb-3">
+            <Button
+              onClick={() => {
+                navigate("/exploreDao");
+              }}
+              variant="outline"
+              className="text-white bg-purple-600"
+            >
+              My Communities
+            </Button>
+            <Button
+              onClick={() => {
+                navigate("/exploreDao");
+              }}
+              variant="outline"
+              className="text-white bg-purple-600"
+            >
+              Explore DAO
+            </Button>
+          </div>
+          <Card className=" w-full flex flex-col items-center p-5 text-2xl font-bold  mt-2 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30  max-w-[1440px]  ">
+            Currently there is no Activity by User
+          </Card>
+        </div>
       </div>
     </div>
   );
