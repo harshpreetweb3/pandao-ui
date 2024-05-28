b3a47f0f6f7f29440e4c3b40c22847e90b13d70a
faisal004 <faisalhusain1320@gmail.com>
2024-05-28 19:47:13 +0530

    added : skeleton added

--- frontend/src/Pages/exploreDao.jsx
+++ frontend/src/Pages/exploreDao.jsx
@@ -3,21 +3,24 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import BackButton from "./components/BackButton";
+import { Skeleton } from "@/components/ui/skeleton"
+
 
 
 const ExploreDao = () => {
   const navigate = useNavigate();
   const { accounts } = useAccount();
   const [data, setData] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchBluePrint = async () => {
       try {
-        const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/blueprint`
-        );
+        const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/blueprint`);
         setData(res.data);
       } catch (error) {
         console.error("Error fetching blueprint data:", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchBluePrint();
@@ -31,19 +34,33 @@ const ExploreDao = () => {
     <div className="pt-20 pb-10  items-start gap-3 justify-start min-h-screen bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-[#281038] from-0% via-[#181734] via-50%  to-[#0D1E3B] to-100% text-black px-2">
      <BackButton/>
       <div className="max-w-[1440px]  grid md:grid-cols-3 grid-cols-1 mx-auto p-3 gap-3">
-      
-      {data && data.map((project, index) => (
-          <div
-            key={index}
-            onClick={() => {
-              navigate(`/exploreDao/${project.slug}`);
-            }}
-            className="bg-purple-400 shadow-md rounded-lg p-6 hover:bg-purple-300 cursor-pointer"
-          >
-            <h2 className="text-2xl font-bold mb-2">{project.slug}</h2>
-            <p className="text-gray-900 mb-4 line-clamp-3">{project.description}</p>
-          </div>
-        ))}
+   
+
+      {loading ? (
+          <>
+            <Skeleton className="w-[450px] h-[200px] bg-purple-300" />
+            <Skeleton className="w-[450px] h-[200px] bg-purple-300" />
+            <Skeleton className="w-[450px] h-[200px] bg-purple-300" />
+            <Skeleton className="w-[450px] h-[200px] bg-purple-300" />
+            <Skeleton className="w-[450px] h-[200px] bg-purple-300" />
+            <Skeleton className="w-[450px] h-[200px] bg-purple-300" />
+
+         
+          </>
+        ) : (
+          data.map((project, index) => (
+            <div
+              key={index}
+              onClick={() => {
+                navigate(`/exploreDao/${project.slug}`);
+              }}
+              className="bg-purple-400 shadow-md rounded-lg p-6 hover:bg-purple-300 cursor-pointer"
+            >
+              <h2 className="text-2xl font-bold mb-2">{project.slug}</h2>
+              <p className="text-gray-900 mb-4 line-clamp-3">{project.description}</p>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
