4d32fbd77e6f5827ba7c0d93959cf7a5dc2db34f
faisal004 <faisalhusain1320@gmail.com>
2024-06-21 21:54:44 +0530

    css fix

--- frontend/src/Pages/Explore/CreateYouOwnDoa.jsx
+++ frontend/src/Pages/Explore/CreateYouOwnDoa.jsx
@@ -1,5 +1,5 @@
 const Createyourowndao = () => {
-    return ( <div className="min-h-28">
+    return ( <div className="min-h-28 flex items-center justify-center">
         Coming Soon
     </div> );
 }
