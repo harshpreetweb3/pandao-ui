6ee2b6d0176006438b2348ee6c4c647ccf86d194
faisal004 <faisalhusain1320@gmail.com>
2024-08-07 22:00:32 +0530

    changes

--- frontend/src/Pages/UserDashboard.jsx
+++ frontend/src/Pages/UserDashboard.jsx
@@ -6,10 +6,20 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
-import { Activity, Check, ClockIcon, Copy, Globe } from "lucide-react";
+import {
+  Activity,
+  Check,
+  ChevronLeft,
+  ClockIcon,
+  Copy,
+  Globe,
+  SquareArrowOutDownLeft,
+  SquareArrowOutUpRight,
+} from "lucide-react";
 import { FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
 import { clipAddress } from "@/utils/functions/ClipAddress";
 import { formatStandardDateTime } from "@/utils/functions/convertActivityData";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const UserDashboard = () => {
   const { accounts } = useAccount();
@@ -22,6 +32,7 @@ const UserDashboard = () => {
   const [copied, setCopied] = useState(false);
   const [data, setData] = useState([]);
   const [userData, setUserData] = useState({});
+  const [createdComminities,setCreatedCommunities]=useState([])
   // const [edit, setEdit] = useState(false);
   // const [fileUrl, setFileUrl] = useState("");
   const [about, setAbout] = useState("");
@@ -90,11 +101,23 @@ const UserDashboard = () => {
         console.error("Error fetching user data:", error);
       }
     };
+    const fetchCreatedCommunity = async (user_address) => {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/user/community/${user_address}?owner=false`,
+        
+        );
+       setCreatedCommunities(res.data)
+      } catch (error) {
+        console.error("Error fetching activity data:", error);
+      }
+    };
 
     if (accounts && accounts.length > 0 && accounts[0].address) {
       fetchBluePrint();
       fetchUserData();
       fetchActivity(accounts[0].address, 1, 10);
+      fetchCreatedCommunity(accounts[0].address)
     }
   }, [accounts, currentPage, pageSize]);
 
@@ -110,75 +133,88 @@ const UserDashboard = () => {
 
   return (
     <div className="pt-20 relative flex items-start gap-3 justify-start min-h-screen bg-slate-100 text-black p-7">
-      {userData && (
-        <div className="flex flex-col w-full">
-          <Card className="w-full flex md:flex-row flex-col shadow-md items-center md:items-start max-w-[1200px] mx-auto rounded-sm  text-black border-b-0 h-48">
-          {userData.usermetadata?.cover_url && (
-            <img
-             src={coverUrl || "/Pandao.png"}
-              alt="Cover"
-              className="aspect-video h-48 w-full object-cover"
-            />)}
-          </Card>
-          <Card className="w-full rounded-t-none border-t-0 flex md:flex-row flex-col shadow-md items-center md:items-start max-w-[1200px] mx-auto rounded-sm p-5 text-black ">
-            <CardHeader className="p-0 -translate-y-32 ">
-              <Avatar className="h-72 w-72 border-[5px] border-purple-400">
-                {userData.usermetadata?.image_url && (
-                  <AvatarImage
-                    src={userData.usermetadata.image_url}
-                    className="h-72 w-72 object-cover"
+      <Tabs defaultValue="overview" className="w-full  t ">
+        <TabsList className=" w-full flex  items-center justify-start  max-w-[1400px] mx-auto  ">
+          <div
+            onClick={() => navigate(-1)}
+            className="bg-white rounded-md p-1 flex items-center justify-center mr-2"
+          >
+            <ChevronLeft />
+          </div>
+          <TabsTrigger value="overview">Overview</TabsTrigger>
+          <TabsTrigger value="activity">User Activity</TabsTrigger>
+        </TabsList>
+        <TabsContent value="overview">
+          {userData && (
+            <div className="flex flex-col w-full">
+              <Card className="w-full flex md:flex-row flex-col shadow-md items-center md:items-start max-w-[1400px] mx-auto rounded-sm  text-black border-b-0 h-48">
+                {userData.usermetadata?.cover_url && (
+                  <img
+                    src={coverUrl || "/Pandao.png"}
+                    alt="Cover"
+                    className="aspect-video h-48 w-full object-cover"
                   />
                 )}
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </CardHeader>
-            <CardContent className="text-xl  relative font-bold flex flex-col gap-2 w-full p-5 justify-between">
-              <div className="flex flex-col gap-1">
-                <div className="text-black font-semibold text-3xl">
-                  {userData.name}
-                </div>
-                <div className="text-black font-light text-base text-left px-0">
-                  {userData.usermetadata?.about}
-                </div>
-                <div className="py-1 w-[300px]  rounded-sm flex flex-wrap text-ellipsis overflow-hidden relative group">
-                  {clipAddress(accounts[0].address)}
-                  <button
-                    onClick={() => handleCopy(accounts[0].address)}
-                    disabled={copied}
-                    className="py-0  text-black rounded-md px-2 h-6 text-xs absolute top-2 -right-2 group-hover:block "
-                  >
-                    {copied ? (
-                      <Check className="h-4 w-4" />
-                    ) : (
-                      <Copy className="h-4 w-4" />
+              </Card>
+              <Card className="w-full rounded-t-none border-t-0 flex md:flex-row flex-col shadow-md items-center md:items-start max-w-[1400px] mx-auto rounded-sm p-5 text-black ">
+                <CardHeader className="p-0 -translate-y-32 ">
+                  <Avatar className="h-72 w-72 border-[5px] border-purple-400">
+                    {userData.usermetadata?.image_url && (
+                      <AvatarImage
+                        src={userData.usermetadata.image_url}
+                        className="h-72 w-72 object-cover"
+                      />
                     )}
-                  </button>
-                </div>
-                {socialLinks.x_url && (
-                  <div className="flex items-center w-full gap-2">
-                    <FaXTwitter className="text-black" />
-                    <div className="px-3 py-1 text-sm w-full bg-transparent  text-black">
-                      {socialLinks.x_url}
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </CardHeader>
+                <CardContent className="text-xl  relative font-bold flex flex-col gap-2 w-full p-5 justify-between">
+                  <div className="flex flex-col gap-1">
+                    <div className="text-black font-semibold text-3xl">
+                      {userData.name}
                     </div>
-                  </div>
-                )}
-                {socialLinks.website_url && (
-                  <div className="flex items-center w-full gap-2">
-                    <Globe className="text-black" />
-                    <div className="px-3 py-1 text-sm w-full bg-transparent  text-black">
-                      {socialLinks.website_url}
+                    <div className="text-black font-light text-base text-left px-0">
+                      {userData.usermetadata?.about}
                     </div>
-                  </div>
-                )}
-                {socialLinks.linkedin && (
-                  <div className="flex items-center w-full gap-2">
-                    <FaLinkedinIn className="text-black" />
-                    <div className="px-3 py-1 text-sm w-full bg-transparent  text-black">
-                      {socialLinks.linkedin}
+                    <div className="py-1 w-[300px]  rounded-sm flex flex-wrap text-ellipsis overflow-hidden relative group">
+                      {clipAddress(accounts[0].address)}
+                      <button
+                        onClick={() => handleCopy(accounts[0].address)}
+                        disabled={copied}
+                        className="py-0  text-black rounded-md px-2 h-6 text-xs absolute top-2 -right-2 group-hover:block "
+                      >
+                        {copied ? (
+                          <Check className="h-4 w-4" />
+                        ) : (
+                          <Copy className="h-4 w-4" />
+                        )}
+                      </button>
                     </div>
-                  </div>
-                )}{" "}
-                {/* {socialLinks.tiktok && (
+                    {socialLinks.x_url && (
+                      <div className="flex items-center w-full gap-2">
+                        <FaXTwitter className="text-black" />
+                        <div className="px-3 py-1 text-sm w-full bg-transparent  text-black">
+                          {socialLinks.x_url}
+                        </div>
+                      </div>
+                    )}
+                    {socialLinks.website_url && (
+                      <div className="flex items-center w-full gap-2">
+                        <Globe className="text-black" />
+                        <div className="px-3 py-1 text-sm w-full bg-transparent  text-black">
+                          {socialLinks.website_url}
+                        </div>
+                      </div>
+                    )}
+                    {socialLinks.linkedin && (
+                      <div className="flex items-center w-full gap-2">
+                        <FaLinkedinIn className="text-black" />
+                        <div className="px-3 py-1 text-sm w-full bg-transparent  text-black">
+                          {socialLinks.linkedin}
+                        </div>
+                      </div>
+                    )}{" "}
+                    {/* {socialLinks.tiktok && (
                   <div className="flex items-center w-full gap-2">
                     <FaTiktok className="text-black" />
                     <div className="px-3 py-1 text-sm w-full bg-transparent  text-black">
@@ -186,18 +222,54 @@ const UserDashboard = () => {
                     </div>
                   </div>
                 )} */}
-              </div>
+                  </div>
 
-              <Button
-                onClick={() => navigate("/userDashboard/edit")}
-                className="bg-slate-700 border-2 absolute right-0 top-2"
-              >
-                Edit Profile
-              </Button>
-            </CardContent>
+                  <Button
+                    onClick={() => navigate("/userDashboard/edit")}
+                    className="bg-slate-700 border-2 absolute right-0 top-2"
+                  >
+                    Edit Profile
+                  </Button>
+                </CardContent>
+              </Card>
+              <div>
+              <Card className="w-full flex mt-1  flex-col shadow-md items-center md:items-start max-w-[1400px] mx-auto rounded-sm p-5 text-black  ">
+            <div className="text-xl font-bold">Created Communities</div>
+            <div className="w-full mt-4 flex flex-col gap-2 ">
+              {createdComminities.map((activity, index) => (
+                <div key={index} className="p-4 border-2 rounded-lg group hover:shadow-md ">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <Avatar className="h-12 w-12 mt-1">
+                        <AvatarImage
+                          src={activity.community_image}
+                          className="h-12 w-12 object-cover"
+                        />
+
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <div className="flex items-center gap-2">
+                          <span>{activity.community_name}</span>
+                        </div>
+                       
+                      </div>
+                    </div>
+                    <div onClick={()=>navigate(`/community/detail/${activity.community_id}`)} className="border-2 rounded-full p-2 group-hover:text-purple-600 group-hover:border-purple-600 cursor-pointer">
+                      <SquareArrowOutUpRight className="h-4 w-4" />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+           
           </Card>
-
-          <Card className="w-full flex mt-1  flex-col shadow-md items-center md:items-start max-w-[1200px] mx-auto rounded-sm p-5 text-black  ">
+              </div>
+            </div>
+          )}
+        </TabsContent>
+        <TabsContent value="activity">
+          <Card className="w-full flex mt-1  flex-col shadow-md items-center md:items-start max-w-[1400px] mx-auto rounded-sm p-5 text-black  ">
             <div className="text-xl font-bold">User Activity</div>
             <div className="w-full mt-4 flex flex-col gap-2 ">
               {activityData.map((activity, index) => (
@@ -242,8 +314,8 @@ const UserDashboard = () => {
               </Button>
             </div> */}
           </Card>
-        </div>
-      )}
+        </TabsContent>
+      </Tabs>
     </div>
   );
 };
